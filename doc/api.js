b3c2cec806cba190cb579bee955b56506ecaeecc
Fabien LOISON <flo@flogisoft.com>
2014-03-18 17:29:45 +0100

    Updated from master

--- doc/api.js
+++ doc/api.js
@@ -14,6 +14,7 @@ YUI.add("yuidoc-meta", function(Y) {
         "photonui.Dialog",
         "photonui.FAIcon",
         "photonui.Field",
+        "photonui.FluidLayout",
         "photonui.GridLayout",
         "photonui.Helpers",
         "photonui.Label",
